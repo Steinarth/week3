97e94395324c23d734ceaf4f55d33382d026cdc1
Steinar Þ <steinart16@ru.is>
2017-12-14 02:09:08 +0000

    tests and implementation of ticCell, need to figure out how to render without error

--- client/src/tictactoe/TicCell.js
+++ client/src/tictactoe/TicCell.js
@@ -17,6 +17,16 @@ export default function (injected) {
         componentWillMount(){
             this.unsubscribe = eventRouter.on('MovePlaced', (moveEvent)=>{
             //    Key logic goes here. Remember---the cell gets all move events, not only its own.
+          
+          // Find the correct cell and mark that one.
+          if(this.props.coordinates.x === moveEvent.move.xy.x &&
+               this.props.coordinates.y === moveEvent.move.xy.y){
+                 // setState() enqueues changes to the component state and tells React that the component need re-rendering with new state
+                 this.setState((oldstate, props) =>{
+                    return {move: {side: moveEvent.move.side}};
+                  });
+
+               }
             })
         }
         componentWillUnmount(){
--- client/src/tictactoe/TicCell.spec.js
+++ client/src/tictactoe/TicCell.spec.js
@@ -3,7 +3,6 @@ import { shallow } from 'enzyme';
 
 import TicCellComponent from './TicCell';
 
-
 import MessageRouter from '../common/framework/message-router';
 
 
@@ -34,13 +33,61 @@ describe("Tic Cell", function () {
         component = shallow(<TicCell coordinates={{x:1,y:2}} gameId="thegame" />, div);
     });
 
+    function given() {
+        //noinspection JSUnusedAssignment
+        let side, xy, gameId="thegame";
+        let api = {
+            side(aside){
+                side = aside;
+                return api;
+            },
+            gameId(agameId){
+                gameId=agameId;
+                return api;
+            },
+            xy(axy){
+                xy = axy;
+                return api;
+            },
+            xy(axy){
+                xy = axy;
+                return api;
+            },
+            placed(){
+                eventRouter.routeMessage({
+                    type: "MovePlaced",
+                    gameId: gameId,
+                    move: {
+                        side: side,
+                        xy: xy
+                    }
+                });
+                return api;
+            }
+        };
+        return api;
+
+    }
+
 
     it('should render without error', function () {
-
+      // TODO::::
+      //  expect(component.state('error')).toBe(false);
     });
 
-    it('should do more stuff', function () {
+    it('should render cell [1,2] on click to be O', function () {
+      given().gameId("someOtherGame").side("O").xy({x:1, y:2}).placed();
+      expect(component.text()).toBe("O");
+    })
 
+    it('should render cell [1,2] on click to be X', function () {
+      given().gameId("someOtherGame").side("X").xy({x:1, y:2}).placed();
+      expect(component.text()).toBe("X");
+    })
+
+    it('Should start with empty cell on [3,3]', function () {
+      var something = shallow(<TicCell coordinates={{x:3,y:3}} gameId="someGame" />, div);
+      expect(something.text()).toBe("");
     })
 
 });
